42280c338309b6e55456dd8e473fad3958fb0c4d
Rafael Deras <rafael.deras@gmail.com>
2026-09-02 10:58:52 +0000

    Initial commit

--- catolicomm/misas/urls.py
+++ catolicomm/misas/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "misas"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("templos/<uuid:templo_id>/", views.templo, name="templo"), 
+    path("templo/<uuid:templo_id>/", views.templo, name="templo"), 
+    path("templo/nuevo/", views.templo, name="nuevo_templo"),
 ]
--- catolicomm/misas/views.py
+++ catolicomm/misas/views.py
@@ -14,3 +14,20 @@ def index(request):
 def templo(request, templo_id):
     templo = get_object_or_404(Templo, pk=templo_id)
     return render(request, 'misas/templo.html', {'templo': templo})
+
+def nuevo_templo(request):
+    nuevo_templo=Templo()
+    nuevo_templo.nombre=request.POST["nombre_templo"]
+    nuevo_templo.direccion=request.POST["direccion_templo"]
+    nuevo_templo.alias=request.POST["alias_templo"]
+
+    try:
+        nuevo_templo.save()
+    except Exception as e:
+        render(
+            request,
+            "misas/newtemplo.html",
+            {
+                "error_message": f"Error al registrar el templo {e.message}"
+            }
+        )
